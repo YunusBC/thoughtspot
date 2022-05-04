40e4d072479873af81acb457dcc31adce1d89831
Ozcan KOC <ozcan.k@blue.cloud>
2022-05-04 12:53:40 +0300

    added real_estate_agent icon

--- src/components/TopMenuNav/TopMenuNav.js
+++ src/components/TopMenuNav/TopMenuNav.js
@@ -1,10 +1,38 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./index.css";
 
 const TopMenuNav = () => {
   return (
-    <div className="page-TopMenuNav goUp">
-      <div className="page-TopMenuNav-inner"> TopMenuNav123</div>
+    <div className="goUp">
+      <ul className="topMenu">
+        <li className="top-nav-item active open">
+          <Link to="/cards2">
+            <i className="material-icons">how_to_reg</i>
+            <span>Facility Management</span>
+            <span className="selected"></span>
+          </Link>
+        </li>
+        <li className="top-nav-item">
+          <Link to="/searchbar" href="#">
+            <i className="material-icons">recent_actors</i>
+            <span>Lease Management</span>
+          </Link>
+        </li>
+        <li className="top-nav-item">
+          <Link to="/searchbar" href="#">
+            <i className="material-icons">real_estate_agent</i>
+            <span>Property Management</span>
+          </Link>
+        </li>
+
+        <li className="top-nav-item">
+          <Link to="/reports">
+            <i className="material-icons">group</i>
+            <span>Personal Liveboards</span>
+          </Link>
+        </li>
+      </ul>
     </div>
   );
 };
