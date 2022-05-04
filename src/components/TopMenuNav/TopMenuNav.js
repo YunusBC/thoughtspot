e42f456f873ddde4ce3813c430c19be21212755f
Ozcan KOC <ozcan.k@blue.cloud>
2022-05-04 12:16:20 +0300

    topMenuNav added

---
+++ src/components/TopMenuNav/TopMenuNav.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./index.css";
+
+const TopMenuNav = () => {
+  return (
+    <div className="page-TopMenuNav goUp">
+      <div className="page-TopMenuNav-inner"> TopMenuNav123</div>
+    </div>
+  );
+};
+
+export default TopMenuNav;
