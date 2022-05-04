3227c30ed5875a62033d69b9595e3c4665b40031
Ozcan KOC <ozcan.k@blue.cloud>
2022-05-04 23:49:29 +0300

    top header

--- src/components/TopMenuNav/TopMenuNav.js
+++ src/components/TopMenuNav/TopMenuNav.js
@@ -1,49 +1,33 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import "./index.css";
 
-import {
-  BrowserRouter,
-  Routes, // instead of "Switch"
-  Route,
-  useLocation,
-} from "react-router-dom";
-
 function TopMenuWithoutNav() {
-  return <div>Now showing post </div>;
+  return (
+    <div className="goUp">
+      <h2 className="breadCrumbHeader">Facility Management</h2>
+    </div>
+  );
 }
 
 const TopMenuNav1 = () => {
   return (
     <div className="goUp">
-      <ul className="topMenu">
-        <li className="top-nav-item active open">
-          <Link to="/cards2">
-            <i className="material-icons">how_to_reg</i>
-            <span>Facility Management</span>
-            <span className="selected"></span>
-          </Link>
-        </li>
-        <li className="top-nav-item">
-          <Link to="/searchbar" href="#">
-            <i className="material-icons">recent_actors</i>
-            <span>Lease Management</span>
-          </Link>
-        </li>
-        <li className="top-nav-item">
-          <Link to="/searchbar" href="#">
-            <i className="material-icons">real_estate_agent</i>
-            <span>Property Management</span>
-          </Link>
-        </li>
-
-        <li className="top-nav-item">
-          <Link to="/reports">
-            <i className="material-icons">group</i>
-            <span>Personal Liveboards</span>
-          </Link>
-        </li>
-      </ul>
+      <div class="five item menu">
+        <Link className="item" to="/facility">
+          Facility Management
+        </Link>
+        <Link className="item" to="/lease">
+          Lease Management
+        </Link>
+        <Link className="item" to="/property">
+          Property Management
+        </Link>
+        <Link className="item" to="/liveboards">
+          Personal Liveboards
+        </Link>
+        <Link className="item" to="/cards2"></Link>
+      </div>
     </div>
   );
 };
