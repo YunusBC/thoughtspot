295cb9857b1a7ab81b51fd8b3c7b6d6b7aa9bde3
Yunus Burakgazi <yunus.b@blue.cloud>
2022-05-05 15:33:21 +0300

    kpi card urls in homepage are updated

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -14,15 +14,15 @@ const Home = () => {
             </div>
             <div className="column">
                 <iframe
-                        title="Total Work Orders"
-                        src="https://blue-cloud.thoughtspot.cloud/#/embed/viz/8af809cb-88c3-4586-97ab-e062088d84e9/2c2f8672-3e53-4970-b0ab-c6f692bf9831"
+                        title="Lease Expiring"
+                        src="https://blue-cloud.thoughtspot.cloud/#/embed/viz/b7489cf1-9581-4fab-9c46-292549772aa3/22b25a53-bd48-496c-aa3b-3b867bdfd2bc"
                         frameBorder="0">
                 </iframe>
             </div>
             <div className="column">
                 <iframe
-                        title="Total Work Orders"
-                        src="https://blue-cloud.thoughtspot.cloud/#/embed/viz/8af809cb-88c3-4586-97ab-e062088d84e9/2c2f8672-3e53-4970-b0ab-c6f692bf9831"
+                        title="Total Properties"
+                        src="https://blue-cloud.thoughtspot.cloud/#/embed/viz/b7489cf1-9581-4fab-9c46-292549772aa3/20dfe116-19ba-46a6-a7b1-fe24a9959a91"
                         frameBorder="0">
                 </iframe>
             </div>
