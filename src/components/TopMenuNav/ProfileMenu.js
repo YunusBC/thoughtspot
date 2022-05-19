af479fae9a66cdd8460d1996d26219d011e2de7a
Ozcan KOC <ozcankoc99@gmail.com>
2022-05-19 15:27:31 +0300

    remove dead code

--- src/components/TopMenuNav/ProfileMenu.js
+++ src/components/TopMenuNav/ProfileMenu.js
@@ -1,6 +1,5 @@
 import React from "react";
 import "./index.css";
-import { Icon } from "semantic-ui-react";
 
 const ProfileMenu = () => {
   return (
--- src/components/TopMenuNav/TopMenuNav.js
+++ src/components/TopMenuNav/TopMenuNav.js
@@ -1,5 +1,4 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import "./index.css";
 import SettingsContext from "../stateManagement/SettingsContext";
 import ProfileMenu from "./ProfileMenu";
