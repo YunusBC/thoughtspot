fa6ab78209425073b8c9520644e2d9fd922af602
Ozcan KOC <ozcankoc99@gmail.com>
2022-05-19 15:29:14 +0300

    alt

--- src/components/TopMenuNav/ProfileMenu.js
+++ src/components/TopMenuNav/ProfileMenu.js
@@ -5,17 +5,18 @@ const ProfileMenu = () => {
   return (
     <div className="profile-menu two wide column four-icons">
       <div class="ui icon borderless compact menu ">
-        <a class="item">
+        <a href="/" class="item">
           <i class="bell icon"></i>
         </a>
-        <a class="item">
+        <a href="/" class="item">
           <i class="video camera icon"></i>
         </a>
-        <a class="item">
+        <a href="/" class="item">
           <i class="sign-out icon"></i>
           <img
             class="user-profile ui rounded image "
             src={require("../../assets/img/user.png")}
+            alt=""
           ></img>
         </a>
       </div>
