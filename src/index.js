ac489ac4ea34925844e4a23e212201a636049975
Ozcan KOC <ozcankoc99@gmail.com>
2022-05-24 19:10:14 +0300

    fonts

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import 'semantic-ui-css/semantic.min.css'
-
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import "./index.css";
+import "semantic-ui-css/semantic.min.css";
 ReactDOM.render(
   <React.StrictMode>
-    <App/>
+    <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
