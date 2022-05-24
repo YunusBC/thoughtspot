8a9d8385bac9be1c3680b176a12788ed5f65f755
Yunus Burakgazi <yunus.b@blue.cloud>
2022-05-24 22:21:14 +0300

    implemented hover effect on left menu items

--- src/components/LeftMenu/menu.js
+++ src/components/LeftMenu/menu.js
@@ -21,9 +21,9 @@ import "./menu.css";
             <img src={topLeftFullLogo} alt="blue cloud"></img>
         </div>
         <div className="bgColor row">
-          <div></div>
-          <div>
-            <div className={`${context.settings.menuItem === 'facility' ? 'active' : 'hover'} `} onClick={() => {context.selectMenu('facility'); handleItemClick('facility')}}>
+          <div className='paddingTop50px'></div>
+          <div className={`${context.settings.menuItem === 'facility' ? 'active' : 'hover'} itemContainer`}>
+            <div className='menu' onClick={() => {context.selectMenu('facility'); handleItemClick('facility')}}>
               <div className='menuIconDiv'>
                 <i className="flaticon-facility-management"/>
               </div>
@@ -31,73 +31,66 @@ import "./menu.css";
                 <p className='menuTitle'>Facility Management</p>
               </div>
             </div>
-          </div>
-          <div>
-            <div className={`${context.settings.menuItem === 'lease' ? 'active' : 'hover'} `} onClick={() => {context.selectMenu('lease'); handleItemClick('lease')}}>
-              <div className='menuIconDiv'>
-                <i className="flaticon-contract"/>
+            </div>
+            <div className={`${context.settings.menuItem === 'lease' ? 'active' : 'hover'} itemContainer`}>
+              <div className='menu' onClick={() => {context.selectMenu('lease'); handleItemClick('lease')}}>
+                <div className='menuIconDiv'>
+                  <i className="flaticon-contract"/>
+                </div>
+                <div className='menuText'>
+                  <p className='menuTitle'>Lease Management</p>
+                </div>
               </div>
-              <div className='menuText'>
-                <p className='menuTitle'>Lease Management</p>
+            </div>
+            <div className={`${context.settings.menuItem === 'property' ? 'active' : 'hover'} itemContainer`}>
+              <div className='menu' onClick={() => {context.selectMenu('property'); handleItemClick('property')}}>
+                <div className='menuIconDiv'>
+                  <i className="flaticon-property"/>
+                </div>
+                <div className='menuText'>
+                  <p className='menuTitle'>Property Management</p>
+                </div>
+              </div>
+            </div>
+            <div className={`${context.settings.menuItem === 'liveboards' ? 'active' : 'hover'} itemContainer`}>
+              <div className='menu' onClick={() => {context.selectMenu('liveboards'); handleItemClick('liveboards')}}>
+                <div className='menuIconDiv'>
+                  <i className="flaticon-display"/>
+                </div>
+                <div className='menuText'>
+                  <p className='menuTitle'>Personal Liveboards</p>
+                </div>
               </div>
             </div>
           </div>
-          <div>
-            <div className={`${context.settings.menuItem === 'property' ? 'active' : 'hover'} `} onClick={() => {context.selectMenu('property'); handleItemClick('property')}}>
-              <div className='menuIconDiv'>
-                <i className="flaticon-property"/>
-              </div>
-              <div className='menuText'>
-                <p className='menuTitle'>Property Management</p>
-              </div>
-            </div>
-          </div>
-          <div>
-            <div className={`${context.settings.menuItem === 'liveboards' ? 'active' : 'hover'} `} onClick={() => {context.selectMenu('liveboards'); handleItemClick('liveboards')}}>
-              <div className='menuIconDiv'>
-                <i className="flaticon-display"/>
-              </div>
-              <div className='menuText'>
-                <p className='menuTitle'>Personal Liveboards</p>
-              </div>
-            </div>
-          </div>
-        </div>
         </>
       :
       <>
         <div className='topLeftDivCollapsed' onClick={() => handleItemClick('')}>
             <img src={topLeftPartialLogo} alt="blue cloud"></img>
         </div>
-        <div className="bgColor row">   
-          <div>
-            <div className={`${context.settings.menuItem === 'facility' ? 'activeCollapsed' : 'hoverCollapsed'} `} onClick={() => {context.selectMenu('facility'); handleItemClick('facility')}}>
-              <div className='menuIconDiv'>
-                <i className="flaticon-facility-management"/>
+        <div className="bgColorCollapsed row">
+        <div className='paddingTop50px'></div>  
+          <div className={`${context.settings.menuItem === 'facility' ? 'activeCollapsed' : 'hoverCollapsed'} itemContainerCollapsed`}>
+              <div className='collapsedMenu' onClick={() => {context.selectMenu('facility'); handleItemClick('facility')}}>
+                  <i className="flaticon-facility-management"/>
+                </div>
+            </div>
+            <div className={`${context.settings.menuItem === 'lease' ? 'activeCollapsed' : 'hoverCollapsed'} itemContainerCollapsed`}>
+              <div className='collapsedMenu' onClick={() => {context.selectMenu('lease'); handleItemClick('lease')}}>
+                  <i className="flaticon-contract"/>
               </div>
             </div>
-          </div>
-          <div>
-            <div className={`${context.settings.menuItem === 'lease' ? 'activeCollapsed' : 'hoverCollapsed'} `} onClick={() => {context.selectMenu('lease'); handleItemClick('lease')}}>
-              <div className='menuIconDiv'>
-                <i className="flaticon-contract"/>
+            <div className={`${context.settings.menuItem === 'property' ? 'activeCollapsed' : 'hoverCollapsed'} itemContainerCollapsed`}>
+              <div className='collapsedMenu' onClick={() => {context.selectMenu('property'); handleItemClick('property')}}>
+                  <i className="flaticon-property"/>
               </div>
             </div>
-          </div>
-          <div>
-            <div className={`${context.settings.menuItem === 'property' ? 'activeCollapsed' : 'hoverCollapsed'} `} onClick={() => {context.selectMenu('property'); handleItemClick('property')}}>
-              <div className='menuIconDiv'>
-                <i className="flaticon-property"/>
+            <div className={`${context.settings.menuItem === 'liveboards' ? 'activeCollapsed' : 'hoverCollapsed'} itemContainerCollapsed`}>
+              <div className='collapsedMenu' onClick={() => {context.selectMenu('liveboards'); handleItemClick('liveboards')}}>
+                  <i className="flaticon-display"/>
               </div>
             </div>
-          </div>
-          <div>
-            <div className={`${context.settings.menuItem === 'liveboards' ? 'activeCollapsed' : 'hoverCollapsed'} `} onClick={() => {context.selectMenu('liveboards'); handleItemClick('liveboards')}}>
-              <div className='menuIconDiv'>
-                <i className="flaticon-display"/>
-              </div>
-            </div>
-          </div>
         </div>
       </>
       )
