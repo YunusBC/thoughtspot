6f544bb83476357bdcbd1d7a35de0ecc6afb8df5
Yunus Burakgazi <yunus.b@blue.cloud>
2022-05-25 22:45:55 +0300

    increased kpi cards height at home page

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -22,14 +22,14 @@ useEffect(() => {
         <div className="Divblock">
             <div className="displayBlock">
                 <div className="displayInlineBlock">
-                    <iframe width="98%" height="275px"
+                    <iframe width="98%" height="350px"
                             title="Total Work Orders"
                             src="https://blue-cloud.thoughtspot.cloud/#/embed/viz/b7489cf1-9581-4fab-9c46-292549772aa3/618d1d5f-a7eb-492c-a238-8624a984024a"
                             frameBorder="0">
                     </iframe>
                 </div>
                 <div className="displayInlineBlock">
-                    <iframe width="98%" height="275px"
+                    <iframe width="98%" height="350px"
                             title="Total Work Orders"
                             src="https://blue-cloud.thoughtspot.cloud/#/embed/viz/b7489cf1-9581-4fab-9c46-292549772aa3/3f1c3804-563e-4d4f-9ed5-89516a0a3f3a"
                             frameBorder="0">
@@ -38,14 +38,14 @@ useEffect(() => {
             </div>
             <div className="displayBlock">
                 <div className="displayInlineBlock">
-                    <iframe width="98%" height="275px"
+                    <iframe width="98%" height="350px"
                             title="Total Work Orders"
                             src="https://blue-cloud.thoughtspot.cloud/#/embed/viz/b7489cf1-9581-4fab-9c46-292549772aa3/aea8d1c3-ddcd-4479-8223-7f464995d2d8"
                             frameBorder="0">
                     </iframe>
                 </div>
                 <div className="displayInlineBlock">
-                    <iframe width="98%" height="275px"
+                    <iframe width="98%" height="350px"
                             title="Total Work Orders"
                             src="https://blue-cloud.thoughtspot.cloud/#/embed/viz/b7489cf1-9581-4fab-9c46-292549772aa3/22b25a53-bd48-496c-aa3b-3b867bdfd2bc"
                             frameBorder="0">
