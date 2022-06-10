8b28baf4a5c0bf845c9f5f9f638b32311d6e75f5
Ozcan KOC <ozcankoc99@gmail.com>
2022-06-10 14:18:13 +0300

    parent height fixed

--- src/components/BlueCloudHome/BlueCloudHomePage.js
+++ src/components/BlueCloudHome/BlueCloudHomePage.js
@@ -2,7 +2,7 @@ import React, { useEffect, useContext } from "react";
 import "./index.css";
 import BcHeader from "./BcHeader";
 import BcFeatures from "./BcFeatures";
-import SettingsContext from "../stateManagement/SettingsContext"
+import SettingsContext from "../stateManagement/SettingsContext";
 
 const BlueCloudHomePage = () => {
   const context = useContext(SettingsContext);
@@ -11,19 +11,18 @@ const BlueCloudHomePage = () => {
     context.setIsLandingPage(false);
   }, []);
   return (
-    <>
-      <div className="parent">
-        <div className="header grid-area">
-          <BcHeader />
-        </div>
-        <div className="main grid-area">Main</div>
-        <div className="sidebar grid-area">Sidebar</div>
-        <div className="features grid-area">
-          <BcFeatures />
-        </div>
-        <div className="footer grid-area"> </div>
+    <div className="parent">
+      <div className="header grid-area">
+        <BcHeader />
       </div>
-    </>
+      <div className="main grid-area"> </div>
+      <div className="features grid-area">
+        <BcFeatures />
+      </div>
+      <div className="bcfooter grid-area">
+        © Copyright 2022 Blue.cloud Inc | All Rights Reserved
+      </div>
+    </div>
   );
 };
 
